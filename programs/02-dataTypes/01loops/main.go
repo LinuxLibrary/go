7bb4c4a710ff4021a852bfc80f843afa43dd9038
Arjun Shrinivas <vmsnivas@gmail.com>
2019-07-22 22:10:39 +0530

    Added programs/

---
+++ programs/02-dataTypes/01loops/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Everyone,
+		Hakuna Matata!")
+	fmt.Println("Lets try Iterating / Looping")
+	for i:= 0; i < 100; i++ {
+		fmt.Println(i)
+	}
+}
