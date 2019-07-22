da5b18f0b8199f498bc0c9484a21fbe5173cd478
Arjun Shrinivas <vmsnivas@gmail.com>
2019-07-22 23:34:49 +0530

    Modified programs/02-dataTypes/01loops/main.go

--- programs/02-dataTypes/01loops/main.go
+++ programs/02-dataTypes/01loops/main.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Everyone,
-		Hakuna Matata!")
+	fmt.Println("Hello Everyone",
+		"Hakuna Matata!")
+	foo()
+}
+
+func foo() {
+	fmt.Println("I'm in foo!")
+
 	fmt.Println("Lets try Iterating / Looping")
-	for i:= 0; i < 100; i++ {
-		fmt.Println(i)
+	fmt.Println("Printing even numbers...")
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
 	}
+
 }
