0441148fd50bf120d5ee41d6a47fa051cac303b3
Arjun Shrinivas <vmsnivas@gmail.com>
2019-08-02 23:16:18 +0530

    Modified programs/05-exercisesNL2/main01.go

--- programs/05-exercisesNL2/main01.go
+++ programs/05-exercisesNL2/main01.go
@@ -7,6 +7,7 @@ import (
 var a int = 47
 
 func main() {
+	fmt.Println("Print a number in Decimal, Binary and HexaDecimal")
 	fmt.Printf("Decimal value of a: %v\n", a)
 	fmt.Printf("Binary value of a: %b\n", a)
 	fmt.Printf("HexaDecimal value of a: %#X\n", a)
