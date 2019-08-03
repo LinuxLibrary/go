5655a961ad062af96a5449a64d12113ab3eb610b
Arjun Shrinivas <vmsnivas@gmail.com>
2019-08-03 18:44:02 +0530

    Added programs/src/

---
+++ programs/src/05-exercisesNL2/main03.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a        = "Untyped: Hakuna Matata"
+	b string = "String Type: Hakuna Matata"
+)
+
+func main() {
+	fmt.Println("Declare and print Untyped and typed constant")
+	fmt.Println(a)
+	fmt.Println(b)
+}
---
+++ programs/src/06-controlFlow/01loops/01loopIntro/main01.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("For loop can be used in the following way!\n")
+	fmt.Println("for init; condition; post {")
+	fmt.Println("	fmt.Println(\"Perform Action\")")
+	fmt.Println("}")
+}
