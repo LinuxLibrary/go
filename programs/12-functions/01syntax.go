1aa1ff11413eb8c117062662980f432883b2a0ee
Arjun Shrinivas <vmsnivas@gmail.com>
2019-08-12 21:29:06 +0530

    Modified programs/12-functions/01syntax.go

--- programs/12-functions/01syntax.go
+++ programs/12-functions/01syntax.go
@@ -8,6 +8,10 @@ func main() {
 	foo()
 	bar("Geek!")
 	s1 := woo("Moneypenny")
+	fmt.Println(s1)
+	x, y := mouse("Ian", "Flemings")
+	fmt.Println(x)
+	fmt.Println(y)
 }
 
 // func (r receiver) identifier(parameters) (return(s)) { ... }
@@ -20,6 +24,12 @@ func bar(s string) {
 	fmt.Println("Hello,", s)
 }
 
-func woo(st string) {
-	return fmt.Sprint("Hello from woo,", st)
+func woo(st string) string {
+	return fmt.Sprint("Hello from woo, ", st)
+}
+
+func mouse(fn string, ln string) (string, bool) {
+	a := fmt.Sprint(fn, ln, `, says "Hello!"`)
+	b := false
+	return a, b
 }
